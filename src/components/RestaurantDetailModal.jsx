470e2704b9ab382151137afb064da4d1357b856b
gogo1414 <wnstn6945@gmail.com>
2024-10-07 17:07:56 +0900

    feat: Restaurant 관련된 모달 창 구현

---
+++ src/components/RestaurantDetailModal.jsx
@@ -0,0 +1,20 @@
+import styles from '../css/RestaurantDetailModal.module.css';
+
+function RestaurantDetailModal() {
+    return (
+        <div className={styles["modal modal--open"]}>
+            <div class="modal-backdrop"></div>
+            <div class="modal-container">
+                <h2 class="modal-title text-title">음식점 이름</h2>
+                <div class="restaurant-info">
+                    <p class="restaurant-info__description text-body">음식점 소개 문구</p>
+                </div>
+                <div class="button-container">
+                    <button class="button button--primary text-caption">닫기</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default RestaurantDetailModal;
